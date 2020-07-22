244cd9a8441f5ad2d63dc799aa4e8c946878da74
Fabrício Oliveira <fabriciololiveira@gmail.com>
2020-07-22 04:46:46 -0300

    Create Tabs component

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>Clock</div>
+  <v-card>
+    <v-tabs v-model="timerType">
+      <v-tab
+        v-for="tab in tabTitles"
+        :key="tab"
+      >
+        {{ tab }}
+      </v-tab>
+    </v-tabs>
+  </v-card>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+        timerType: 0,
+        tabTitles: ['Pomodoro', 'Short Break', 'Long Break']
+    }
+  }
+}
+</script>
